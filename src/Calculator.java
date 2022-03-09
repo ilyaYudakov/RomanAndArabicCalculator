9a8e00572469457e63ae6e1d2ecc1ed5c7918255
ilya Yudakov <ilyuno@ya.ru>
2022-03-09 13:06:05 +0300

    add and fix calculate metod

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
     private String firstValue;
     private String secondValue;
     private String operation;
+    private Boolean isRoman = false;
 
     public void inputData() {
 
@@ -17,54 +18,93 @@ public class Calculator {
         secondValue = values[2];
     }
 
-    public void calculate(int a, char b, int c){
+    public Integer[] stringToInteger(String firstValue, String secondValue) {
+        Integer[] values = new Integer[2];
+        if (isRomanNumber(firstValue) && isRomanNumber(secondValue)) {
+            isRoman = true;
+            values[0] = romanToInt(firstValue);
+            values[1] = romanToInt(secondValue);
+        } else {
+            try {
+                values[0] = Integer.valueOf(firstValue);
+                values[1] = Integer.valueOf(secondValue);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("It is impossible to perform arithmetic operations of Roman and Arabic numerals together");
+            }
+        }
+        return values;
+    }
 
-        switch (b){
+    public String integerToString(Integer value) {
+        if (isRoman) {
+            if (value <= 0) {
+                throw new RuntimeException("Roman numerals can't be zero or negative");
+            }
+            return  intToRoman(value);
+        }
+        return String.valueOf(value);
+    }
 
-            case '+':
-                System.out.println(a + c);
-                break;
-            case '-':
-                System.out.println(a - c);
-                break;
-            case '*':
-                System.out.println(a * c);
-            case '/':
-                System.out.println(a / c);
-            default:
-                System.out.println("Invalid ");
+    private Boolean isRomanNumber(String value) {
+        return (value.contains("I") || value.contains("V") || value.contains("X"));
+    }
+
+    public void calculate(String firstValue, String secondValue, String operation) {
+
+        Integer[] converterValues = stringToInteger(firstValue, secondValue);
+        Integer num1 = converterValues[0];
+        Integer num2 = converterValues[1];
+        if (num1 > 10 || num2 > 10) {
+            throw new RuntimeException("You can't perform operations with numbers greater than 10");
+        }
+        try {
+            switch (operation) {
+                case "+":
+                    return integerToString(num1 + num2);
+                case "-":
+                    return integerToString(num1 - num2);
+                case "*":
+                    return integerToString(num1 * num2);
+                case "/":
+                    return integerToString(num1 / num2);
+                default:
+                    throw new RuntimeException("Unsupported operation");
+            }
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Can't be divided by zero");
         }
     }
 
-    public String intToRoman(int num) {
-        int[] values = {1, 5, 10};
-        String[] roman = {"I", "V", "X"};
+    public String intToRoman(Integer value) {
+        int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100};
+        String[] roman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C"};
         StringBuilder sb = new StringBuilder();
-        for (int i = values.length - 1; i >= 0 && num > 0; i--) {
-            while (num >= values[i]) {
-                num -= values[i];
+        for (int i = values.length - 1; i >= 0 && value > 0; i--) {
+            while (value >= values[i]) {
+                value -= values[i];
                 sb.append(roman[i]);
             }
         }
         return sb.toString();
     }
 
-    public int romanToInt(String str) {
+    public int romanToInt(String value) {
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
 
-        int result = map.get(str.charAt(str.length() - 1));
-        for (int i = str.length() - 2; i >= 0; i--) {
-            if (map.get(str.charAt(i)) < map.get(str.charAt(i + 1))) {
-                result -= map.get(str.charAt(i));
+        int result = map.get(value.charAt(value.length() - 1));
+        for (int i = value.length() - 2; i >= 0; i--) {
+            if (map.get(value.charAt(i)) < map.get(value.charAt(i + 1))) {
+                result -= map.get(value.charAt(i));
             } else {
-                result += map.get(str.charAt(i));
+                result += map.get(value.charAt(i));
             }
         }
         return result;
     }
+
     public String getFirstValue() {
         return firstValue;
     }
